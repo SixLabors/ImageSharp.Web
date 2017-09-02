af71dd79f5530bf8054f19b9ee22827825cf3970
Scott Williams <tocsoft@gmail.com>
2017-09-02 12:14:30 +0100

    update command parser tests to make sue of theories

--- tests/ImageSharp.Web.Tests/Commands/CommandParserTests.cs
+++ tests/ImageSharp.Web.Tests/Commands/CommandParserTests.cs
@@ -13,197 +13,79 @@ namespace SixLabors.ImageSharp.Web.Tests.Commands
 {
     public class CommandParserTests
     {
-        [Fact]
-        public void CommandParsesIntegralValues()
+        public static TheoryData<object, string> IntegralValues = new TheoryData<object, string> {
+            { (sbyte)1, "1" },
+            { (byte)1, "1" },
+            { (short)1, "1" },
+            { (ushort)1, "1" },
+            { (int)1, "1" },
+            { (uint)1, "1" },
+            { (long)1, "1" },
+            { (ulong)1, "1" },
+            { (float)1, "1" },
+            { (double)1, "1" },
+            { (decimal)1, "1" },
+        };
+
+        const double Pi = 3.14159265358979;
+        static string PiString = Pi.ToString(CultureInfo.InvariantCulture);
+        static double RoundedPI = Math.Round(Pi, MidpointRounding.AwayFromZero);
+        public static TheoryData<object, string> RealValues = new TheoryData<object, string> {
+            { (sbyte)RoundedPI, PiString },
+            { (byte)RoundedPI, PiString},
+            { (short)RoundedPI, PiString },
+            { (ushort)RoundedPI, PiString },
+            { (int)RoundedPI, PiString },
+            { (uint)RoundedPI, PiString },
+            { (long)RoundedPI, PiString },
+            { (ulong)RoundedPI, PiString },
+            { (float)Pi, PiString },
+            { (double)Pi, PiString},
+            { (decimal)Pi, PiString},
+        };
+
+        public static TheoryData<object, string> EnumValues = new TheoryData<object, string> {
+            { ResizeMode.Max, "max" },
+            { ResizeMode.Crop, "this is not an enum value" }, // unknown returns defualt
+        };
+
+        public static TheoryData<object, string> IntegralArrays = new TheoryData<object, string> {
+            { new[] { 1, 2, 3, 4 }, "1,2,3,4" },
+        };
+
+        public static TheoryData<object, string> RealArrays = new TheoryData<object, string> {
+            { new float[] { 1.667F, 2.667F, 3.667F, 4.667F }, "1.667,2.667,3.667,4.667" },
+        };
+
+        public static TheoryData<object, string> IntegralLists = new TheoryData<object, string> {
+            { new List<int> { 1, 2, 3, 4 }, "1,2,3,4" },
+        };
+
+        public static TheoryData<object, string> RealLists = new TheoryData<object, string> {
+            { new List<float> { 1.667F, 2.667F, 3.667F, 4.667F }, "1.667,2.667,3.667,4.667" },
+        };
+
+        public static TheoryData<object, string> Rgba32Values = new TheoryData<object, string> {
+            {Rgba32.White , "255,255,255" },
+            {Rgba32.Transparent , "255,255,255,0" }, // ??is this right??
+            {Rgba32.Orange , "orange" },
+            {Rgba32.Lime , "00FF00" },
+        };
+
+        [Theory]
+        [MemberData(nameof(IntegralValues))]
+        [MemberData(nameof(RealValues))]
+        [MemberData(nameof(EnumValues))]
+        [MemberData(nameof(IntegralArrays))]
+        [MemberData(nameof(RealArrays))]
+        [MemberData(nameof(IntegralLists))]
+        [MemberData(nameof(RealLists))]
+        [MemberData(nameof(Rgba32Values))]
+        public void CommandParses<T>(T expected, string param)
         {
-            string param = "1";
-
-            sbyte sb = CommandParser.Instance.ParseValue<sbyte>(param);
-            Assert.NotNull(sb);
-            Assert.Equal((sbyte)1, sb);
-
-            byte b = CommandParser.Instance.ParseValue<byte>(param);
-            Assert.NotNull(b);
-            Assert.Equal((byte)1, b);
-
-            short s = CommandParser.Instance.ParseValue<short>(param);
-            Assert.NotNull(s);
-            Assert.Equal((short)1, s);
-
-            ushort us = CommandParser.Instance.ParseValue<ushort>(param);
-            Assert.NotNull(us);
-            Assert.Equal((ushort)1, us);
-
-            int i = CommandParser.Instance.ParseValue<int>(param);
-            Assert.NotNull(i);
-            Assert.Equal(1, i);
-
-            uint ui = CommandParser.Instance.ParseValue<uint>(param);
-            Assert.NotNull(ui);
-            Assert.Equal(1u, ui);
-
-            long l = CommandParser.Instance.ParseValue<long>(param);
-            Assert.NotNull(l);
-            Assert.Equal(1L, l);
-
-            ulong ul = CommandParser.Instance.ParseValue<ulong>(param);
-            Assert.NotNull(i);
-            Assert.Equal(1uL, ul);
-
-            float f = CommandParser.Instance.ParseValue<float>(param);
-            Assert.NotNull(f);
-            Assert.Equal(1F, f);
-
-            double d = CommandParser.Instance.ParseValue<double>(param);
-            Assert.NotNull(d);
-            Assert.Equal(1D, d);
-
-            decimal m = CommandParser.Instance.ParseValue<decimal>(param);
-            Assert.NotNull(m);
-            Assert.Equal(1M, m);
-        }
-
-        [Fact]
-        public void CommandParsesRealValues()
-        {
-            // Math.PI.ToString() chops two digits off the end
-            const double Pi = 3.14159265358979;
-            string param = Pi.ToString(CultureInfo.InvariantCulture);
-            double rounded = Math.Round(Pi, MidpointRounding.AwayFromZero);
-
-            sbyte sb = CommandParser.Instance.ParseValue<sbyte>(param);
-            Assert.NotNull(sb);
-            Assert.Equal((sbyte)rounded, sb);
-
-            byte b = CommandParser.Instance.ParseValue<byte>(param);
-            Assert.NotNull(b);
-            Assert.Equal((byte)rounded, b);
-
-            short s = CommandParser.Instance.ParseValue<short>(param);
-            Assert.NotNull(s);
-            Assert.Equal((short)rounded, s);
-
-            ushort us = CommandParser.Instance.ParseValue<ushort>(param);
-            Assert.NotNull(us);
-            Assert.Equal((ushort)rounded, us);
-
-            int i = CommandParser.Instance.ParseValue<int>(param);
-            Assert.NotNull(i);
-            Assert.Equal((int)rounded, i);
-
-            uint ui = CommandParser.Instance.ParseValue<uint>(param);
-            Assert.NotNull(i);
-            Assert.Equal((uint)rounded, ui);
-
-            long l = CommandParser.Instance.ParseValue<long>(param);
-            Assert.NotNull(l);
-            Assert.Equal((long)rounded, l);
-
-            ulong ul = CommandParser.Instance.ParseValue<ulong>(param);
-            Assert.NotNull(i);
-            Assert.Equal((ulong)rounded, ul);
-
-            float f = CommandParser.Instance.ParseValue<float>(param);
-            Assert.NotNull(f);
-            Assert.Equal((float)Pi, f);
-
-            double d = CommandParser.Instance.ParseValue<double>(param);
-            Assert.NotNull(d);
-            Assert.Equal(Pi, d);
-
-            decimal m = CommandParser.Instance.ParseValue<decimal>(param);
-            Assert.NotNull(m);
-            Assert.Equal((decimal)Pi, m);
-        }
-
-        [Fact]
-        public void CommandParsesEnums()
-        {
-            string param = "max";
-
-            ResizeMode mode = CommandParser.Instance.ParseValue<ResizeMode>(param);
-
-            Assert.NotNull(mode);
-            Assert.Equal(ResizeMode.Max, mode);
-
-            // Check default is returned.
-            param = "hjbjhbjh";
-            mode = CommandParser.Instance.ParseValue<ResizeMode>(param);
-
-            Assert.NotNull(mode);
-            Assert.Equal(ResizeMode.Crop, mode);
-        }
-
-        [Fact]
-        public void CommandParsesIntegralArrays()
-        {
-            string param = "1,2,3,4";
-
-            int[] actual = CommandParser.Instance.ParseValue<int[]>(param);
-
-            Assert.NotNull(actual);
-            Assert.Equal(new[] { 1, 2, 3, 4 }, actual);
-        }
-
-        [Fact]
-        public void CommandParsesRealArrays()
-        {
-            string param = "1.667,2.667,3.667,4.667";
-
-            float[] actual = CommandParser.Instance.ParseValue<float[]>(param);
-
-            Assert.NotNull(actual);
-            Assert.Equal(new[] { 1.667F, 2.667F, 3.667F, 4.667F }, actual);
-        }
-
-        [Fact]
-        public void CommandParsesIntegralLists()
-        {
-            string param = "1,2,3,4";
-
-            List<int> actual = CommandParser.Instance.ParseValue<List<int>>(param);
-
-            Assert.NotNull(actual);
-            Assert.Equal(new List<int> { 1, 2, 3, 4 }, actual);
-        }
-
-        [Fact]
-        public void CommandParsesRealLists()
-        {
-            string param = "1.667,2.667,3.667,4.667";
-
-            List<float> actual = CommandParser.Instance.ParseValue<List<float>>(param);
-
-            Assert.NotNull(actual);
-            Assert.Equal(new List<float> { 1.667F, 2.667F, 3.667F, 4.667F }, actual);
-        }
-
-        [Fact]
-        public void CommandParsesRgba32()
-        {
-            string param = "255,255,255";
-            Rgba32 actual = CommandParser.Instance.ParseValue<Rgba32>(param);
-
-            Assert.NotNull(actual);
-            Assert.Equal(Rgba32.White, actual);
-
-            string param2 = "255,255,255,0";
-            Rgba32 actual2 = CommandParser.Instance.ParseValue<Rgba32>(param2);
-
-            Assert.NotNull(actual2);
-            Assert.Equal(Rgba32.Transparent, actual2);
-
-            string param3 = "orange";
-            Rgba32 actual3 = CommandParser.Instance.ParseValue<Rgba32>(param3);
-
-            Assert.NotNull(actual3);
-            Assert.Equal(Rgba32.Orange, actual3);
-
-            string param4 = "00FF00";
-            Rgba32 actual4 = CommandParser.Instance.ParseValue<Rgba32>(param4);
-
-            Assert.NotNull(actual4);
-            Assert.Equal(Rgba32.Lime, actual4);
+            T sb = CommandParser.Instance.ParseValue<T>(param);
+            Assert.IsType<T>(sb);
+            Assert.Equal(expected, sb);
         }
     }
 }
