1ec0003f0153d85a345fbb43ad5245028c2fe923
James Jackson-South <james_south@hotmail.com>
2017-10-12 20:17:50 +1100

    Add test server

---
+++ tests/ImageSharp.Web.Tests/StaticFilesTestServer.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ImageSharp.Web.Tests
+{
+    public static class StaticFilesTestServer
+    {
+        public static TestServer Create(Action<IApplicationBuilder> configureApp, Action<IServiceCollection> configureServices = null)
+        {
+            void DefaultConfigureServices(IServiceCollection services)
+            {
+            }
+
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(new[]
+                {
+                    new KeyValuePair<string, string>("webroot", ".")
+                })
+                .Build();
+
+            IWebHostBuilder builder = new WebHostBuilder()
+                .UseConfiguration(configuration)
+                .Configure(configureApp)
+                .ConfigureServices(configureServices ?? DefaultConfigureServices);
+            return new TestServer(builder);
+        }
+    }
+}
