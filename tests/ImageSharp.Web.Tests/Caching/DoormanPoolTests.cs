3cb8271f0b64f86182563e1ef13eec4d41ec2936
James Jackson-South <james_south@hotmail.com>
2018-04-02 22:32:20 +1000

    Better DoormanPool Test

--- tests/ImageSharp.Web.Tests/Caching/DoormanPoolTests.cs
+++ tests/ImageSharp.Web.Tests/Caching/DoormanPoolTests.cs
@@ -21,22 +21,15 @@ namespace SixLabors.ImageSharp.Web.Tests.Caching
         [Fact]
         public void DoormanPoolReusesItems()
         {
-            int initialCount = DoormanPool.Count();
             Doorman first = DoormanPool.Rent();
 
-            int currentCount = DoormanPool.Count();
-            if (currentCount > 0)
-            {
-                Assert.Equal(initialCount - 1, currentCount);
-                DoormanPool.Return(first);
-                Assert.Equal(initialCount, DoormanPool.Count());
-            }
-            else
-            {
-                Assert.Equal(0, currentCount);
-                DoormanPool.Return(first);
-                Assert.Equal(initialCount + 1, DoormanPool.Count());
-            }
+            DoormanPool.Return(first);
+
+            Doorman second = DoormanPool.Rent();
+
+            Assert.Equal(first, second);
+
+            DoormanPool.Return(second);
         }
 
         [Fact]
