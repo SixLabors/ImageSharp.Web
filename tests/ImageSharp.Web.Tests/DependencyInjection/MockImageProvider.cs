5d381f065d1e178d5b2048ec4f08df0869a4e3ff
James Jackson-South <james_south@hotmail.com>
2019-08-04 23:15:39 +1000

    Query collection to remove. Fix #62 (#66)
    * Query collection to remove. Fix #62
    
    * Lock Azure Storage creation
    
    * Use Empty try...catch

---
+++ tests/ImageSharp.Web.Tests/DependencyInjection/MockImageProvider.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using SixLabors.ImageSharp.Web.Providers;
+using SixLabors.ImageSharp.Web.Resolvers;
+using System;
+using System.Threading.Tasks;
+
+namespace SixLabors.ImageSharp.Web.Tests.DependencyInjection
+{
+    public class MockImageProvider : IImageProvider
+    {
+        public Func<HttpContext, bool> Match { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Task<IImageResolver> GetAsync(HttpContext context) => throw new NotImplementedException();
+
+        public bool IsValidRequest(HttpContext context) => throw new NotImplementedException();
+    }
+}
