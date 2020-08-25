89ce0fdf34e21d4ffb6b28d8bb519316339a0710
James Jackson-South <james_south@hotmail.com>
2020-08-25 21:48:17 +0100

    Attempt to run Action tests on Linux

--- tests/ImageSharp.Web.Tests/Actions/ActionTests.cs
+++ tests/ImageSharp.Web.Tests/Actions/ActionTests.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Six Labors.
 // Licensed under the Apache License, Version 2.0.
 
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.TestHost;
@@ -12,35 +11,13 @@ namespace SixLabors.ImageSharp.Web.Tests.Actions
 {
     public class ActionTests
     {
+        private const string Command = "?width=20";
+
         [Theory]
         [InlineData(ImageSharpTestServer.PhysicalTestImage)]
         [InlineData(ImageSharpTestServer.AzureTestImage)]
         public async Task ShouldRunOnValidateActionAsync(string url)
         {
-            // Running this test with Azurite https://github.com/Azure/Azurite/ on Travis results in the following exception.
-            //
-            // Error Message:
-            // Microsoft.Azure.Storage.StorageException : Unexpected response code, Expected:PartialContent, Received:OK
-            // Stack Trace:
-            //   at Microsoft.Azure.Storage.Core.Executor.Executor.ExecuteAsync[T](RESTCommand`1 cmd, IRetryPolicy policy, OperationContext operationContext, CancellationToken token)
-            //   at Microsoft.Azure.Storage.Blob.BlobReadStream.DispatchReadAsync(Byte[] buffer, Int32 offset, Int32 count, CancellationToken cancellationToken)
-            //   at Microsoft.Azure.Storage.Blob.BlobReadStream.Read(Byte[] buffer, Int32 offset, Int32 count)
-            //   at SixLabors.ImageSharp.Image.InternalDetectFormat(Stream stream, Configuration config)
-            //   at SixLabors.ImageSharp.Image.DiscoverDecoder(Stream stream, Configuration config, IImageFormat& format)
-            //   at SixLabors.ImageSharp.Image.Decode[TPixel](Stream stream, Configuration config)
-            //   at SixLabors.ImageSharp.Image.Load[TPixel](Configuration config, Stream stream, IImageFormat& format)
-            //   at SixLabors.ImageSharp.Web.Middleware.ImageSharpMiddleware.Invoke(HttpContext context) in /home/travis/build/SixLabors/ImageSharp.Web/src/ImageSharp.Web/Middleware/ImageSharpMiddleware.cs:line 228
-            //   at Microsoft.AspNetCore.Hosting.Internal.RequestServicesContainerMiddleware.Invoke(HttpContext httpContext)
-            //   at Microsoft.AspNetCore.TestHost.ClientHandler.<>c__DisplayClass3_0.<<SendAsync>b__0>d.MoveNext()
-            // --- End of stack trace from previous location where exception was thrown ---
-            //   at Microsoft.AspNetCore.TestHost.ClientHandler.SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-            //   at System.Net.Http.HttpClient.FinishSendAsyncBuffered(Task`1 sendTask, HttpRequestMessage request, CancellationTokenSource cts, Boolean disposeCts)
-            //   at SixLabors.ImageSharp.Web.Tests.Actions.ActionTests.ShouldRunOnValidateAction(String url) in /home/travis/build/SixLabors/ImageSharp.Web/tests/ImageSharp.Web.Tests/Actions/ActionTests.cs:line 31
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            {
-                // return;
-            }
-
             bool complete = false;
             void OnParseCommands(ImageCommandContext context)
             {
@@ -53,7 +30,7 @@ namespace SixLabors.ImageSharp.Web.Tests.Actions
 
             using (TestServer server = ImageSharpTestServer.CreateWithActions(OnParseCommands))
             {
-                await server.CreateClient().GetAsync(url + "?width=20");
+                await server.CreateClient().GetAsync(url + Command);
             }
 
             Assert.True(complete);
@@ -63,11 +40,6 @@ namespace SixLabors.ImageSharp.Web.Tests.Actions
         [InlineData(ImageSharpTestServer.AzureTestImage)]
         public async Task ShouldRunOnValidateActionNoCommandsAsync(string url)
         {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            {
-               // return;
-            }
-
             bool complete = false;
             void OnParseCommands(ImageCommandContext context)
             {
@@ -100,7 +72,7 @@ namespace SixLabors.ImageSharp.Web.Tests.Actions
 
             using (TestServer server = ImageSharpTestServer.CreateWithActions(null, OnBeforeSave))
             {
-                await server.CreateClient().GetAsync(ImageSharpTestServer.PhysicalTestImage + "?width=20");
+                await server.CreateClient().GetAsync(ImageSharpTestServer.PhysicalTestImage + Command);
             }
 
             Assert.True(complete);
@@ -121,7 +93,7 @@ namespace SixLabors.ImageSharp.Web.Tests.Actions
 
             using (TestServer server = ImageSharpTestServer.CreateWithActions(null, null, OnProcessed))
             {
-                await server.CreateClient().GetAsync(ImageSharpTestServer.PhysicalTestImage + "?width=20");
+                await server.CreateClient().GetAsync(ImageSharpTestServer.PhysicalTestImage + Command);
             }
 
             Assert.True(complete);
@@ -140,7 +112,7 @@ namespace SixLabors.ImageSharp.Web.Tests.Actions
 
             using (TestServer server = ImageSharpTestServer.CreateWithActions(null, null, null, OnPrepareResponse))
             {
-                await server.CreateClient().GetAsync(ImageSharpTestServer.PhysicalTestImage + "?width=20");
+                await server.CreateClient().GetAsync(ImageSharpTestServer.PhysicalTestImage + Command);
             }
 
             Assert.True(complete);
