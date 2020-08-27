bea1d6555cbc34d4cf7ecd6384e7973f0243ca36
James Jackson-South <james_south@hotmail.com>
2020-08-27 16:27:29 +0100

    Simplify middleware workflow.

--- src/ImageSharp.Web.Providers.Azure/Resolvers/AzureBlobStorageImageResolver.cs
+++ src/ImageSharp.Web.Providers.Azure/Resolvers/AzureBlobStorageImageResolver.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 using Azure;
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
-using Microsoft.IO;
 
 namespace SixLabors.ImageSharp.Web.Resolvers.Azure
 {
@@ -16,22 +15,13 @@ namespace SixLabors.ImageSharp.Web.Resolvers.Azure
     /// </summary>
     public class AzureBlobStorageImageResolver : IImageResolver
     {
-        private readonly RecyclableMemoryStreamManager memoryStreamManager;
         private readonly BlobClient blob;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="AzureBlobStorageImageResolver"/> class.
         /// </summary>
         /// <param name="blob">The Azure blob.</param>
-        /// <param name="memoryStreamManager">
-        /// The recyclable memorystream manager used for managing pooled stream
-        /// buffers independently from image buffer pooling.
-        /// </param>
-        public AzureBlobStorageImageResolver(BlobClient blob, RecyclableMemoryStreamManager memoryStreamManager)
-        {
-            this.blob = blob;
-            this.memoryStreamManager = memoryStreamManager;
-        }
+        public AzureBlobStorageImageResolver(BlobClient blob) => this.blob = blob;
 
         /// <inheritdoc/>
         public async Task<ImageMetadata> GetMetaDataAsync()
@@ -42,16 +32,6 @@ namespace SixLabors.ImageSharp.Web.Resolvers.Azure
 
         /// <inheritdoc/>
         public async Task<Stream> OpenReadAsync()
-        {
-            // Copy to a MemoryStream first because RetriableStreamImpl
-            // doesn't support Position.
-            Stream blobStream = (await this.blob.DownloadAsync()).Value.Content;
-            var memoryStream = new RecyclableMemoryStream(this.memoryStreamManager);
-
-            await blobStream.CopyToAsync(memoryStream);
-            memoryStream.Seek(0, SeekOrigin.Begin);
-
-            return memoryStream;
-        }
+            => (await this.blob.DownloadAsync()).Value.Content;
     }
 }
