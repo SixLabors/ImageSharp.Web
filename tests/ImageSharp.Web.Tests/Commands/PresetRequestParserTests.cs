6efe471232b59338f6b8a5adb77620256a5f080b
Mark Lagendijk <mark@lagendijk.nl>
2021-05-18 11:06:09 +0200

    Added tests

---
+++ tests/ImageSharp.Web.Tests/Commands/PresetRequestParserTests.cs
@@ -0,0 +1,78 @@
+// Copyright (c) Six Labors.
+// Licensed under the Apache License, Version 2.0.
+
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using SixLabors.ImageSharp.Web.Commands;
+using Xunit;
+
+namespace SixLabors.ImageSharp.Web.Tests.Commands
+{
+    public class PresetRequestParserTests
+    {
+        [Fact]
+        public void PresetRequestParserExtractsCommands()
+        {
+            IDictionary<string, string> expected = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+            {
+                { "width", "400" },
+                { "height", "200" }
+            };
+
+            HttpContext context = CreateHttpContext("?preset=Preset1");
+            IDictionary<string, string> actual = new PresetRequestParser(Options.Create(new PresetRequestParserOptions
+            {
+                Presets = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+                {
+                    { "Preset1", "width=400&height=200" }
+                }
+            })).ParseRequestCommands(context);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void PresetRequestParserCommandsWithoutPresetParam()
+        {
+            IDictionary<string, string> expected = new Dictionary<string, string>();
+
+            HttpContext context = CreateHttpContext("?test=test");
+            IDictionary<string, string> actual = new PresetRequestParser(Options.Create(new PresetRequestParserOptions
+            {
+                Presets = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+                {
+                    { "Preset1", "width=400&height=200" }
+                }
+            })).ParseRequestCommands(context);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void PresetRequestParserCommandsWithoutMatchingPreset()
+        {
+            IDictionary<string, string> expected = new Dictionary<string, string>();
+
+            HttpContext context = CreateHttpContext("?preset=Preset2");
+            IDictionary<string, string> actual = new PresetRequestParser(Options.Create(new PresetRequestParserOptions
+            {
+                Presets = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+                {
+                    { "Preset1", "width=400&height=200" }
+                }
+            })).ParseRequestCommands(context);
+
+            Assert.Equal(expected, actual);
+        }
+
+        private static HttpContext CreateHttpContext(string query)
+        {
+            var httpContext = new DefaultHttpContext();
+            httpContext.Request.Path = "/testwebsite.com/image-12345.jpeg";
+            httpContext.Request.QueryString = new QueryString(query);
+            return httpContext;
+        }
+    }
+}
