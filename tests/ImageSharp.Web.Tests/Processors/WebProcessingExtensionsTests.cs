d64e4301d8ae0b7145f5efdd5e330d9beee62f36
Ronald Barendse <ronald@barend.se>
2021-09-01 22:45:35 +0200

    Add tests for WebProcessingExtensions

---
+++ tests/ImageSharp.Web.Tests/Processors/WebProcessingExtensionsTests.cs
@@ -0,0 +1,54 @@
+// Copyright (c) Six Labors.
+// Licensed under the Apache License, Version 2.0.
+
+using System.Collections.Generic;
+using System.Linq;
+using SixLabors.ImageSharp.Web.Processors;
+using SixLabors.ImageSharp.Web.Tests.DependencyInjection;
+using Xunit;
+
+namespace SixLabors.ImageSharp.Web.Tests.Processors
+{
+    public class WebProcessingExtensionsTests
+    {
+        [Fact]
+        public void WebProcessingExtensions_GetBySupportedCommands()
+        {
+            var processors = new IImageWebProcessor[]
+            {
+                new JpegQualityWebProcessor(),
+                new ResizeWebProcessor(),
+                new BackgroundColorWebProcessor(),
+                new MockWebProcessor()
+            };
+
+            var commands = new List<string>
+            {
+                ResizeWebProcessor.Width,
+                JpegQualityWebProcessor.Quality,
+                ResizeWebProcessor.Height
+            };
+
+            var supportedProcessors = WebProcessingExtensions.GetBySupportedCommands(processors, commands).ToArray();
+
+            Assert.Equal(2, supportedProcessors.Length);
+            Assert.IsType<ResizeWebProcessor>(supportedProcessors[0]);
+            Assert.IsType<JpegQualityWebProcessor>(supportedProcessors[1]);
+        }
+
+        [Fact]
+        public void WebProcessingExtensions_GetBySupportedCommands_Empty()
+        {
+            var processors = new IImageWebProcessor[]
+            {
+                new MockWebProcessor()
+            };
+
+            var commands = new List<string>();
+
+            var supportedProcessors = WebProcessingExtensions.GetBySupportedCommands(processors, commands).ToArray();
+
+            Assert.Empty(supportedProcessors);
+        }
+    }
+}
