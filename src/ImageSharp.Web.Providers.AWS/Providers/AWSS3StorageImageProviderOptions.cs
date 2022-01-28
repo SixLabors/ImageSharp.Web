20890aea500709964fa8ca36c665a4667d96a3a0
Andy Felton <andy.felton@equatedigital.com>
2022-01-28 12:33:34 +0000

    Fixed some comments for keys and endpoints

--- src/ImageSharp.Web.Providers.AWS/Providers/AWSS3StorageImageProviderOptions.cs
+++ src/ImageSharp.Web.Providers.AWS/Providers/AWSS3StorageImageProviderOptions.cs
@@ -22,7 +22,7 @@ namespace SixLabors.ImageSharp.Web.Providers.AWS
     public class AWSS3BucketClientOptions
     {
         /// <summary>
-        /// Gets or sets the AWS region name.
+        /// Gets or sets the AWS region endpoint (us-east-1/us-west-1/ap-southeast-2).
         /// </summary>
         public string Region { get; set; }
 
@@ -32,17 +32,22 @@ namespace SixLabors.ImageSharp.Web.Providers.AWS
         public string BucketName { get; set; }
 
         /// <summary>
-        /// Gets or sets the AWS key - optional.
+        /// Gets or sets the AWS key - Can be used to override keys provided by the environment.
+        /// If deploying inside an EC2 instance AWS keys will already be available via environment
+        /// variables and don't need to be specified. Follow AWS best security practices on  <see href="https://docs.aws.amazon.com/general/latest/gr/aws-access-keys-best-practices.html"/>.
         /// </summary>
         public string AccessKey { get; set; }
 
         /// <summary>
-        /// Gets or sets the AWS secret - optional.
+        /// Gets or sets the AWS secret - Can be used to override keys provided by the environment.
+        /// If deploying inside an EC2 instance AWS keys will already be available via environment
+        /// variables and don't need to be specified. Follow AWS best security practices on  <see href="https://docs.aws.amazon.com/general/latest/gr/aws-access-keys-best-practices.html"/>.
         /// </summary>
         public string AccessSecret { get; set; }
 
         /// <summary>
-        /// Gets or sets the AWS endpoint - override for testing
+        /// Gets or sets the AWS endpoint - used for testing to over region endpoint allowing it
+        /// to be set to localhost.
         /// </summary>
         public string Endpoint { get; set; }
     }
