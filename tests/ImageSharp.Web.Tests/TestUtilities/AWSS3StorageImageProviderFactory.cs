a49660664434a3ef75d4a569d532008faddf8f44
James Jackson-South <james_south@hotmail.com>
2022-02-01 01:08:18 +1100

    Revert "Cleanup"
    This reverts commit 4f38ba174aee989a38a81f957e251b218ab09bb4.

--- tests/ImageSharp.Web.Tests/TestUtilities/AWSS3StorageImageProviderFactory.cs
+++ tests/ImageSharp.Web.Tests/TestUtilities/AWSS3StorageImageProviderFactory.cs
@@ -59,14 +59,15 @@ namespace SixLabors.ImageSharp.Web.Tests.TestUtilities
                 }
                 catch (AmazonS3Exception e)
                 {
-                    // CI tests are run in parallel and can sometimes return a
+                    // CI tests are run in parallel and can sometime return a
                     // false negative for the existance of a bucket.
                     if (string.Equals(e.ErrorCode, "BucketExists"))
                     {
                         return;
                     }
 
-                    throw;
+                    // Temp to capture error code
+                    throw new Exception(e.ErrorCode);
                 }
             }
 
