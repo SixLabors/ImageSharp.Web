ff55d06ab12c8b5323a75088a03a26d470fd78a3
Ronald Barendse <ronald@barend.se>
2022-02-01 16:35:44 +0100

    Do not exclude enum from code coverage

--- src/ImageSharp.Web/Caching/LruCache/ItemDestination.cs
+++ src/ImageSharp.Web/Caching/LruCache/ItemDestination.cs
@@ -1,11 +1,8 @@
 // Copyright (c) Six Labors.
 // Licensed under the Apache License, Version 2.0.
 
-using System.Diagnostics.CodeAnalysis;
-
 namespace SixLabors.ImageSharp.Web.Caching
 {
-    [ExcludeFromCodeCoverage]
     internal enum ItemDestination
     {
         Warm,
