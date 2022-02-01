bd5cc00109d3e2db89aef422942801d977382b51
James Jackson-South <james_south@hotmail.com>
2022-02-01 14:18:07 +1100

    Removed unused implementation

--- tests/ImageSharp.Web.Tests/Caching/CacheKeyTests.cs
+++ tests/ImageSharp.Web.Tests/Caching/CacheKeyTests.cs
@@ -2,7 +2,6 @@
 // Licensed under the Apache License, Version 2.0.
 
 using System;
-using System.Text;
 using Microsoft.AspNetCore.Http;
 using SixLabors.ImageSharp.Web.Caching;
 using SixLabors.ImageSharp.Web.Commands;
@@ -103,34 +102,6 @@ namespace SixLabors.ImageSharp.Web.Tests.Caching
             httpContext.Request.QueryString = new QueryString("?Should-Be=Ignored");
 
             return httpContext;
-        }
-
-        /// <summary>
-        /// Original implementation of cache key creation.
-        /// </summary>
-        public class CacheKeyBaseline : ICacheKey
-        {
-            /// <inheritdoc/>
-            public string Create(HttpContext context, CommandCollection commands)
-            {
-                var sb = new StringBuilder(context.Request.Host.ToString());
-
-                string pathBase = context.Request.PathBase.ToString();
-                if (!string.IsNullOrWhiteSpace(pathBase))
-                {
-                    sb.AppendFormat("{0}/", pathBase);
-                }
-
-                string path = context.Request.Path.ToString();
-                if (!string.IsNullOrWhiteSpace(path))
-                {
-                    sb.Append(path);
-                }
-
-                sb.Append(QueryString.Create(commands));
-
-                return sb.ToString().ToLowerInvariant();
-            }
         }
     }
 }
