1766b4c8f2895a494bc02624cec02c21f77eeb77
Ronald Barendse <ronald@barend.se>
2022-02-09 10:08:27 +0100

    Fix test to expect trailing slash

--- tests/ImageSharp.Web.Tests/Providers/PhysicalFileSystemProviderTests.cs
+++ tests/ImageSharp.Web.Tests/Providers/PhysicalFileSystemProviderTests.cs
@@ -10,23 +10,23 @@ namespace SixLabors.ImageSharp.Web.Tests.Providers
     {
         [Theory]
 #if OS_LINUX
-        [InlineData(null, "wwwroot", "/Users/root/", "/Users/root/wwwroot")]
-        [InlineData(null, "/Users/WebRoot", "/Users/root/", "/Users/WebRoot")]
-        [InlineData("providerFolder", "../Temp", "/Users/this/a/root", "/Users/this/a/root/providerFolder")]
-        [InlineData("../Temp", null, "/Users/this/a/root", "/Users/this/a/Temp")]
-        [InlineData("/Users/WebRoot", null, "/Users/this/a/root", "/Users/WebRoot")]
+        [InlineData(null, "wwwroot", "/Users/root/", "/Users/root/wwwroot/")]
+        [InlineData(null, "/Users/WebRoot", "/Users/root/", "/Users/WebRoot/")]
+        [InlineData("providerFolder", "../Temp", "/Users/this/a/root", "/Users/this/a/root/providerFolder/")]
+        [InlineData("../Temp", null, "/Users/this/a/root", "/Users/this/a/Temp/")]
+        [InlineData("/Users/WebRoot", null, "/Users/this/a/root", "/Users/WebRoot/")]
 #elif OS_OSX
-        [InlineData(null, "wwwroot", "/Users/root/", "/Users/root/wwwroot")]
-        [InlineData(null, "/Users/WebRoot", "/Users/root/", "/Users/WebRoot")]
-        [InlineData("providerFolder", "../Temp", "/Users/this/a/root", "/Users/this/a/root/providerFolder")]
-        [InlineData("../Temp", null, "/Users/this/a/root", "/Users/this/a/Temp")]
-        [InlineData("/Users/WebRoot", null, "/Users/this/a/root", "/Users/WebRoot")]
+        [InlineData(null, "wwwroot", "/Users/root/", "/Users/root/wwwroot/")]
+        [InlineData(null, "/Users/WebRoot", "/Users/root/", "/Users/WebRoot/")]
+        [InlineData("providerFolder", "../Temp", "/Users/this/a/root", "/Users/this/a/root/providerFolder/")]
+        [InlineData("../Temp", null, "/Users/this/a/root", "/Users/this/a/Temp/")]
+        [InlineData("/Users/WebRoot", null, "/Users/this/a/root", "/Users/WebRoot/")]
 #elif OS_WINDOWS
-        [InlineData(null, "wwwroot", "C:/root\\", "C:\\root\\wwwroot")]
-        [InlineData(null, "C:/WebRoot", "C:/root\\", "C:/WebRoot")]
-        [InlineData("providerFolder", "../Temp", "C:/this/a/root", "C:\\this\\a\\root\\providerFolder")]
-        [InlineData("../Temp", null, "C:/this/a/root", "C:\\this\\a\\Temp")]
-        [InlineData("C:/WebRoot", null, "C:/this/a/root", "C:/WebRoot")]
+        [InlineData(null, "wwwroot", "C:/root\\", "C:\\root\\wwwroot\\")]
+        [InlineData(null, "C:/WebRoot", "C:/root\\", "C:/WebRoot\\")]
+        [InlineData("providerFolder", "../Temp", "C:/this/a/root", "C:\\this\\a\\root\\providerFolder\\")]
+        [InlineData("../Temp", null, "C:/this/a/root", "C:\\this\\a\\Temp\\")]
+        [InlineData("C:/WebRoot", null, "C:/this/a/root", "C:/WebRoot\\")]
 #endif
         public void ProvidersRootFromOptions(string providerRoot, string webRootPath, string contentRootPath, string expected)
         {
