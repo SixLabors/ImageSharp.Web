4ce04d72aae16d3d41faff3e86e76d92d8adf2f8
James Jackson-South <james_south@hotmail.com>
2022-03-17 12:53:18 +1100

    Try reading the env var directly.

--- tests/ImageSharp.Web.Tests/TestUtilities/TestEnvironment.cs
+++ tests/ImageSharp.Web.Tests/TestUtilities/TestEnvironment.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Six Labors.
 // Licensed under the Apache License, Version 2.0.
 
+using System;
+
 namespace SixLabors.ImageSharp.Web.Tests.TestUtilities
 {
     internal static class TestEnvironment
@@ -11,7 +13,18 @@ namespace SixLabors.ImageSharp.Web.Tests.TestUtilities
 #if ENV_CI
         internal static bool RunsOnCI => true;
 #else
-        internal static bool RunsOnCI => false;
-#endif
+        internal static bool RunsOnCI
+        {
+            get
+            {
+                // TODO: Something weird going on with our environment constants here.
+                // try reading the environment directly.
+                // https://docs.github.com/en/actions/learn-github-actions/environment-variables
+                string variable = Environment.GetEnvironmentVariable("CI");
+                bool.TryParse(variable, out bool result);
+                return result;
+            }
+        }
     }
+#endif
 }
