144f33df36ed417864fdcd71995c2a1754d63955
James Jackson-South <james_south@hotmail.com>
2022-03-21 22:12:37 +1100

    No need for format processor to return true.

--- src/ImageSharp.Web/Processors/FormatWebProcessor.cs
+++ src/ImageSharp.Web/Processors/FormatWebProcessor.cs
@@ -66,6 +66,6 @@ namespace SixLabors.ImageSharp.Web.Processors
         }
 
         /// <inheritdoc/>
-        public bool RequiresAlphaAwarePixelFormat(CommandCollection commands, CommandParser parser, CultureInfo culture) => true;
+        public bool RequiresAlphaAwarePixelFormat(CommandCollection commands, CommandParser parser, CultureInfo culture) => false;
     }
 }
