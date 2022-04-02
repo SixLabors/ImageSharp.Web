f7c5dae0d57842d013bcc458dc1c338cbfe07370
Ronald Barendse <ronald@barend.se>
2022-04-02 13:59:30 +0200

    Add FileProviderImageProvider

---
+++ src/ImageSharp.Web/Resolvers/FileProviderImageResolver.cs
@@ -0,0 +1,29 @@
+// Copyright (c) Six Labors.
+// Licensed under the Apache License, Version 2.0.
+
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.FileProviders;
+
+namespace SixLabors.ImageSharp.Web.Resolvers
+{
+    /// <summary>
+    /// Provides means to manage image buffers from an <see cref="IFileInfo"/> instance.
+    /// </summary>
+    public sealed class FileProviderImageResolver : IImageResolver
+    {
+        private readonly IFileInfo fileInfo;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileProviderImageResolver"/> class.
+        /// </summary>
+        /// <param name="fileInfo">The file info.</param>
+        public FileProviderImageResolver(IFileInfo fileInfo) => this.fileInfo = fileInfo;
+
+        /// <inheritdoc/>
+        public Task<ImageMetadata> GetMetaDataAsync() => Task.FromResult(new ImageMetadata(this.fileInfo.LastModified.UtcDateTime, this.fileInfo.Length));
+
+        /// <inheritdoc/>
+        public Task<Stream> OpenReadAsync() => Task.FromResult(this.fileInfo.CreateReadStream());
+    }
+}
