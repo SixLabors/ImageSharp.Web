d41b7837c9367259195353d3b0fbf10a05553dc4
James Jackson-South <james_south@hotmail.com>
2022-04-06 00:23:24 +1000

    Try running tests sequentially via base attribute

--- tests/ImageSharp.Web.Tests/TestUtilities/ServerTestBase.cs
+++ tests/ImageSharp.Web.Tests/TestUtilities/ServerTestBase.cs
@@ -14,6 +14,7 @@ using Xunit.Abstractions;
 
 namespace SixLabors.ImageSharp.Web.Tests.TestUtilities
 {
+    [Collection("Server tests")]
     public abstract class ServerTestBase<TFixture> : IClassFixture<TFixture>
         where TFixture : TestServerFixture
     {
