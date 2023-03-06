68a82b589d25837b91d3299f96cd7002d7294108
James Jackson-South <james_south@hotmail.com>
2023-03-06 22:54:26 +1000

    Consistant casing

--- src/ImageSharp.Web/Format.cs
+++ src/ImageSharp.Web/Format.cs
@@ -11,30 +11,30 @@ public static class Format
     /// <summary>
     /// Gets the Bmp encoding format.
     /// </summary>
-    public static FormatCommand Bmp { get; } = new(nameof(Bmp));
+    public static FormatCommand Bmp { get; } = new("bmp");
 
     /// <summary>
     /// Gets the Gif encoding format.
     /// </summary>
-    public static FormatCommand Gif { get; } = new(nameof(Gif));
+    public static FormatCommand Gif { get; } = new("gif");
 
     /// <summary>
     /// Gets the Jpg encoding format.
     /// </summary>
-    public static FormatCommand Jpg { get; } = new(nameof(Jpg));
+    public static FormatCommand Jpg { get; } = new("jpg");
 
     /// <summary>
     /// Gets the Png encoding format.
     /// </summary>
-    public static FormatCommand Png { get; } = new(nameof(Png));
+    public static FormatCommand Png { get; } = new("png");
 
     /// <summary>
     /// Gets the Tga encoding format.
     /// </summary>
-    public static FormatCommand Tga { get; } = new(nameof(Tga));
+    public static FormatCommand Tga { get; } = new("tga");
 
     /// <summary>
     /// Gets the WebP encoding format.
     /// </summary>
-    public static FormatCommand WebP { get; } = new(nameof(WebP));
+    public static FormatCommand WebP { get; } = new("webp");
 }
